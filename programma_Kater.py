32d40ad79a78b9c132616f000f74f7096fe2078e
giiulia <80638268+giiulia@users.noreply.github.com>
2021-06-18 01:46:53 +0200

    Update programma_Kater.py

--- programma_Kater.py
+++ programma_Kater.py
@@ -5,7 +5,7 @@ from sympy import *
 from strumenti_utili_v2 import *
 
 
-#risultati della raccolta dati
+#medie e deviazioni standard
 dist_mB = np.array([
     np.mean(dati.d1),
     np.mean(dati.d2),
@@ -87,7 +87,6 @@ plt.xlabel('distanza di mB dal coltello2 (m)', fontsize=15)
 plt.ylabel('periodo di oscillazione (s)', fontsize=15) 
 
 plt.legend() # aggiungo una legenda
-#plt.savefig('computed_data/retta.pgf')
 plt.savefig('computed_data/punti.png')
 plt.show()
 
@@ -95,7 +94,7 @@ plt.show()
 T = ( (T2[3]*T1[4]) - (T1[3]*T2[4]) )/(T1[4]-T2[4]-T1[3]+T2[3])
 g = 4*np.power(np.pi, 2)*0.994/np.power(T, 2)
 
-_T23, _T24, _T13, _T14 = symbols('_T23 _T24 _T13 _T14', real=True) #raggio sferette
+_T23, _T24, _T13, _T14 = symbols('_T23 _T24 _T13 _T14', real=True)
 formula_funzione = ( (_T23*_T14) - (_T13*_T24) )/( _T14 - _T24 - _T13 + _T23 )
 
 #genera uno scalare a partire dalla formula simbolica scritta sopra
@@ -109,19 +108,19 @@ derivata_T24 = genera_derivata(_T24)
 derivata_T13 = genera_derivata(_T13)
 derivata_T14 = genera_derivata(_T14)
 
-#sigma_T = np.sqrt(s_T2[3]**2 + s_T2[4]**2 + s_T1[3]**2 + s_T1[4]**2)
-sigma_T = np.power(derivata_T23*s_T2[3], 2)+ np.power(derivata_T24*s_T2[4], 2) + np.power(derivata_T13*s_T1[3], 2) + np.power(derivata_T14*s_T1[4], 2)
+#sigma_T = np.sqrt(s_T2[3]**2 + s_T2[4]**2 + s_T1[3]**2 + s_T1[4]**2) formula semplice
+sigma_T = np.power(derivata_T23*s_T2[3], 2)+ np.power(derivata_T24*s_T2[4], 2) + np.power(derivata_T13*s_T1[3], 2) + np.power(derivata_T14*s_T1[4], 2) #formula con le derivate
 sigma_T = np.array(sigma_T, dtype=np.float64) 
 sigma_T = np.sqrt(sigma_T)
-print(sigma_T)
+print("errore su T* è: {}".format(sigma_T))
 
 
-_T= symbols('_T', real=True) #raggio sferette
-formula_funzione2 = 4*np.power(np.pi, 2)*0.994/np.power(_T, 2)
+_T= symbols('_T', real=True) 
+formula_funzione = 4*np.power(np.pi, 2)*0.994/np.power(_T, 2)
 
 #genera uno scalare a partire dalla formula simbolica scritta sopra
 def genera_derivata(incognita):
-    formula_derivata = diff(formula_funzione2, incognita)
+    formula_derivata = diff(formula_funzione, incognita)
     print(formula_derivata)
     derivata = formula_derivata.subs([(_T, T)])
     return derivata
@@ -131,14 +130,14 @@ sigma_g = derivata_T*sigma_T
 
 print("accelerazione di gravita: {}±{}".format(g, sigma_g))
 
-#approssimazione angolo infinitesimo
-_T, _teta= symbols('_T _teta', real=True) #raggio sferette
-formula_funzione2 = 4*np.power(np.pi, 2)*0.994*np.power(1+(_teta**2/16), 2)/np.power(_T, 2)
-
-#genera uno scalare a partire dalla formula simbolica scritta sopra
+#----------------------------CORREZIONE dell'approssimazione di angolo infinitesimo
 teta = np.arcsin(dati.b/dati.a)
 sigma_teta = 0.0008 #rad
 print(teta)
+
+_T, _teta= symbols('_T _teta', real=True) 
+formula_funzione2 = 4*np.power(np.pi, 2)*0.994*np.power(1+(_teta**2/16), 2)/np.power(_T, 2) #correzione al secondo ordine della funzione di prima
+
 def genera_derivata(incognita):
     formula_derivata = diff(formula_funzione2, incognita)
     print(formula_derivata)
@@ -148,24 +147,21 @@ def genera_derivata(incognita):
 derivata_T = genera_derivata(_T)
 derivata_teta = genera_derivata(_teta)
 
-sigma_g = np.power(derivata_T*sigma_T, 2)+np.power(derivata_teta*sigma_teta, 2)
-sigma_g = np.array(sigma_g, dtype=np.float64) 
-sigma_g_casuale = np.sqrt(sigma_g)
-print(sigma_g_casuale)
+sigma_g1 = np.power(derivata_T*sigma_T, 2)+np.power(derivata_teta*sigma_teta, 2)
+sigma_g1 = np.array(sigma_g1, dtype=np.float64) 
+sigma_g1 = np.sqrt(sigma_g1)
+print("l'errore casuale su g1 è: {}".format(sigma_g1))
 
-g_nuova = 4*np.power(np.pi, 2)*0.994*((1+(np.power(teta, 2)/16))**2) / (T**2)
-g_precisissima = 4*np.power(np.pi, 2)*0.994*((1+(np.power(teta, 2)/16)+(9*np.power(teta, 4)/1024)+(25*np.power(teta, 6)/16384))**2) / (T**2)
-Errore_sist = g_precisissima-g_nuova #su g nuova
+g_1 = 4*np.power(np.pi, 2)*0.994*((1+(np.power(teta, 2)/16))**2) / (T**2)
+g_2 = 4*np.power(np.pi, 2)*0.994*((1+(np.power(teta, 2)/16)+(9*np.power(teta, 4)/1024)+(25*np.power(teta, 6)/16384))**2) / (T**2) #correzione al sesto ordine della funzione di prima
+Errore_sist = g_2-g_1 #su g_1
 print("l'errore sistematico sulla g nuova è: {}".format(Errore_sist))
 
-sigma_g_tot = sigma_g_casuale**2 + Errore_sist**2
+sigma_g_tot = sigma_g_1**2 + Errore_sist**2
 sigma_g_tot= np.sqrt(sigma_g_tot)
-print("accelerazione di gravita NUOVA: {}±{}".format(g_nuova, sigma_g_tot))
+print("accelerazione di gravita NUOVA g1: {}±{}".format(g_1, sigma_g_tot))
 
 #=======================    generazione tabelle   =======================
-
-#tabelle distanze
-crea_tabella(dati.d6, 6, "computed_data/tableDistances.tex")
 
 #tabelle periodo 1
 crea_tabella(dati.T11, 11, "computed_data/tablePeriod11.tex")
